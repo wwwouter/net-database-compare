012c29ef163a45893373d2f87a5b7eee8cb4cd06
Wouter Mooij <wouter@1twaalf.nl>
2024-02-06 14:18:22 +0100

    EmployeeProjectRepository

---
+++ NHibernate/EmployeeProjectRepository.cs
@@ -0,0 +1,17 @@
+using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class EmployeeProjectRepository : IEmployeeProjectRepository
+{
+    private readonly ISession _session;
+
+    public EmployeeProjectRepository(ISession session)
+    {
+        _session = session;
+    }
+
+}
